122bc0c487449ed8519f11f7bfc3d54ca2c6354d
BlendAndroid <1059152860@qq.com>
2024-03-03 11:11:34 +0800

    后台代理提醒

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -5,6 +5,8 @@ import { GlobalContext } from '../dataPreferences/common/utils/GlobalContext';
 import PreferencesUtil from '../dataPreferences/common/database/Preferencesutil';
 import Logger from '../dataPreferences/common/utils/Logger';
 import CommonConstants from '../dataPreferences/common/constants/CommonConstants';
+import PreferencesHandler from '../reminderClock/model/database/PreferencesHandler';
+import display from '@ohos.display';
 
 const TAG = '[entryAbility]';
 
@@ -19,6 +21,9 @@ export default class EntryAbility extends UIAbility {
     PreferencesUtil.createFontPreferences(this.context);
     // 设置字体默认大小
     PreferencesUtil.saveDefaultFontSize(CommonConstants.SET_SIZE_NORMAL);
+
+    // 后台代理提醒
+    GlobalContext.getContext().setObject('preference', PreferencesHandler.instance);
   }
 
   // 应用的UIAbility实例已创建，该UIAbility配置为单实例模式，再次调用startAbility()方法启动该UIAbility实例。由于启动的还是原来的UIAbility实例，
@@ -34,9 +39,15 @@ export default class EntryAbility extends UIAbility {
   // UIAbility实例创建完成之后，在进入Foreground之前，系统会创建一个WindowStage。每一个UIAbility实例都对应持有一个WindowStage实例。
   // WindowStage为本地窗口管理器，用于管理窗口相关的内容，例如与界面相关的获焦/失焦、可见/不可见。
   // 可以在onWindowStageCreate回调中，设置UI页面加载、设置WindowStage的事件订阅。
-  onWindowStageCreate(windowStage: window.WindowStage) {
+  async onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     Logger.info(TAG, 'onWindowStageCreate');
+
+    // 添加全局信息
+    let globalDisplay: display.Display = display.getDefaultDisplaySync();
+    GlobalContext.getContext().setObject('globalDisplay', globalDisplay);
+    let preference = GlobalContext.getContext().getObject('preference') as PreferencesHandler;
+    await preference.configure(this.context.getApplicationContext());
 
     // 在onWindowStageCreate(windowStage)中通过loadContent接口设置应用要加载的页面
     windowStage.loadContent('pages/Index', (err, data) => {
